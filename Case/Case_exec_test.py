e094e2d2dffe970aa7734c1cf4c064d438669ef0
vtheno <a2550591@gmail.com>
2017-10-29 13:40:55 +0800

    changed case test

--- Case/Case_exec_test.py
+++ Case/Case_exec_test.py
@@ -2,15 +2,28 @@
 from Case_exec import *
 # of中的重名问题
 # code 中的多行问题 ?? 加一个 parser???
+# 就是缩进的问题...
+# 解决思路1:
+# 写个 emacs 插件,在新buffer里写,然后该位置用读文件的形式解决
+# 然后把相应的 open('xxx').readlines() 替换显示 成文件内容
+# 又多了一堆问题 索性这样将就这吧
 a = 233
-
 of = make_of(globals())
 Case(a) <- \
     Do(
-        of(233) <= """a = "233"
+        of(233) <= \
+"""
+a = "233"
 print type(a)
-print a""",
-        of(243) <= "a+=2;print a",
+print "a"
+def c():
+    print "C"
+    return "c"
+print c()
+""",
+        of(243) <= \
+"a+=2;print a",
     )
 
-print memoryview(a)
+
+#print memoryview(a)
