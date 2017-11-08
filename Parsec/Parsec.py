1aef98853ac140ba98ed70ac71789dfa9ebf21d3
vtheno <a2550591@gmail.com>
2017-11-08 12:52:15 +0800

    Chaneged

--- Parsec/Parsec.py
+++ Parsec/Parsec.py
@@ -30,17 +30,23 @@ class mlist(object):
         return "({} . {})".format(self.value[0],self.value[1])
     def __iter__(self):
         return iter(self.value)
+    def extend(self,mlst):
+        if self.empty():
+            return mlst
+        return mlist(self.fst,self.extend(self.snd,mlst) )
     def __invert__(self):
         tmp = [ [] ]
         def un_construct(mlst):
             if mlst.empty():
                 return []
             else:
-                tmp[0]+=mlst.fst
+                #tmp[0]+=mlst.fst
+                #print mlst.fst
+                tmp[0].extend (mlst.fst)
                 return un_construct(mlst.snd)
         un_construct(self)
         #print tmp
-        return ''.join (tmp[0])
+        return tmp[0]#''.join (tmp[0])
 @TypeCheck(result=mlist,string=str)
 def make_inp(string):
     xxs = list(string)
@@ -70,8 +76,10 @@ def sat(eq_p):
             return fail(lst)
         else:
             r,rs = lst
+            print "sat:",r,rs
             if eq_p(r):
-                return succeed(r)(rs)
+                print "sat:",type(r)
+                return succeed(mlist(r,mlist(None,None)))(rs)
             else:
                 return fail(rs)
     return p
@@ -113,7 +121,23 @@ def then(p1,p2):
             return fail(inp)
         #r = r1
         #r+=r2
-        return succeed( mlist( r1,r2 )  )(rest2)
+        def append(mlst1,mlst2):
+            #tmp1 = ~mlst1
+            #tmp2 = ~mlst2
+            #r = []
+            #r.extend(tmp1)
+            #r.extend(tmp2)
+            #def construct(lst):
+            #    if lst==[]:
+            #        return mlist(None,None)
+            #    return mlist(lst[0],construct(lst[1:]))
+            #print construct(r)
+            #return construct(r)
+            if mlst1.empty():
+                return mlst2
+            return mlist(mlst1.fst,append(mlst1.snd,mlst2))
+        r = append(r1,r2)
+        return succeed(r)(rest2)
         #return succeed(r)(rest2)
         # this is seq  递归遍历 construct = mlist(*[r1,r2]) ,cons a ,b = mlist(a,b)
     return p
@@ -135,7 +159,9 @@ def cons(a,b):
         if r == fail_flag:
             return fail(rs)
     else:
-        r = mlist(~a,mlist(None,None))
+        print "cons:",a,b
+        r = mlist(''.join(~a),mlist(None,None))
+        #print "r:",r
         return succeed(r)(rs)
 def many(l_p):
     @TypeCheck(result=parser,inp=mlist)
@@ -157,21 +183,21 @@ def some(l_p):
 lst = make_inp("abcde")
 nums = make_inp("1234")
 r = sat(lambda a:a=="a")(lst)
-print isinstance(r,parser),r
-print literal('a')(lst)
+#print isinstance(r,parser),r
+#print literal('a')(lst)
 a = literal('a')
 b = literal('b')
 c = literal('c')
-print ( literal('a') |alt| fail)(lst)
-print (fail |alt| literal('a'))(lst)
-print (a |alt| (b |alt| c))(lst)
-print ( (a |alt| b) |alt| c) (lst)
-print (a |then| b)(lst)
-print (a |then| b |then| c) (lst)
-print (a |then| (b |then| c))(lst)
+#print ( literal('a') |alt| fail)(lst)
+#print (fail |alt| literal('a'))(lst)
+#print (a |alt| (b |alt| c))(lst)
+#print ( (a |alt| b) |alt| c) (lst)
+#print (a |then| b)(lst)
+#print (a |then| b |then| c) (lst) 
+#print  (a |then| (b |then| c))(lst)
 print "----------------"
-print "many:",many(a)(make_inp("aaab"))
-print "some:",some(a)(make_inp("aaab"))
+#print "many:",many(a)(make_inp("aaab"))
+#print "some:",some(a)(make_inp("aaab"))
 
 def isdigit(c):
     # eq_p
@@ -181,10 +207,9 @@ numbers = some(num)
 def isalpha(c):
     return 'a' <= c <= 'z' or 'A'<= c <= 'Z'
 word = some(sat(isalpha))
-print num(make_inp("1234"))
-r= (numbers |using| cons)(make_inp("12345"))
-print r
-print word(make_inp("abcdef!"))
+#print num(make_inp("1234"))
+#print (numbers)(make_inp("12345"))
+#print word(make_inp("abcdef!"))
 
 def string(mlst):
     # mlst :: mlist
@@ -194,14 +219,15 @@ def string(mlst):
             return succeed( mlist(None,None) )(inp)
         else:
             x,xs = mlst
-            return ( literal(x) |then| string(xs))(inp)
+            return (( literal(x) |then| string(xs)) |using| cons )(inp)
     return p
-print string(make_inp("abc"))(make_inp("abc,def"))
-print (string(make_inp("abc")) |using| cons)(make_inp("abc,def"))
+#print string(make_inp("abc"))(make_inp("abc,def"))
+
 def snd(x,y):
     # snd (x,y) = y
     # just get rest -> return new parser class 容器
-    return parser(y,"empty")
+    print "snd:",x,"|",y
+    return succeed(y)(x)#mlist(None,None))
 
 @Infix
 def xthen(p1,p2):
@@ -215,7 +241,8 @@ def xthen(p1,p2):
 def fst(x,y):
     # fst (x,y) = x
     # const x y = x
-    return parser(x,"empty")
+    print "fst:",x,y
+    return succeed(x)(y)#mlist(None,None))
 @Infix
 def thenx(p1,p2):
     # p1 :: literal p
@@ -225,26 +252,42 @@ def thenx(p1,p2):
         return ( (p1 |then| p2 ) |using| fst)(inp)
     return p
 
-print xthen(a,b)(make_inp("abcde"))
-print thenx(a,b)(make_inp("abcde"))
+#print xthen(a,b)(make_inp("abcde"))
+#print thenx(a,b)(make_inp("abcde"))
 def value(x,y):
     r = {}
     #print "type:", type(x),type(y)
     #print "x,y:",x,y
     if isinstance(x,str):
         if x!="Fail":
-            r['value'] = x
+            r["Num"]=long(x)
             return succeed(r)(y)
         else:
             return fail(y)
     else:
-        r['value'] = ~x
-        return succeed(r)(y)
-
-def expr(inp):
-    return  (numbers |using| value) (inp)
-
-print expr(make_inp("123"))
+        print "x:",x
+        r["Num"]= long(''.join(~x))
+        return succeed(mlist(r,mlist(None,None)))(y)
+def op(x,y):
+    r = {}
+    if isinstance(x,str):
+        if x!="Fail":
+            r['op']=x
+            return succeed(r)(y)
+        else:
+            return fail(y)
+    else:
+        r['op'] = ''.join(~x)
+        return succeed(mlist(r,mlist(None,None)))(y)
+test = ( literal("[") |xthen| ( numbers |thenx| literal("]")) )
+t =  test (make_inp("[123]"))
+print t
+print t.result
+#print type(t.result.fst)
+plus = ( (numbers|using|value )|then| (literal("+")|using|op) |xthen| (numbers|using|value ))
+#n = make_inp("1+2")
+#print plus(n)
+#print expr(make_inp("(123)"))
 
 # 由于我的TypeCheck只是简单的做类型检查,所以可能有许多麻烦
 # 因为上述的fail 类型是 method ...所以构造会不同
@@ -254,3 +297,4 @@ print expr(make_inp("123"))
 # 理清楚这里的类型 就好做了
 # 区分清楚哪些函数的类型
 # 函数是一个过程 函数参数 转换 到函数返回值 类型的过程
+
